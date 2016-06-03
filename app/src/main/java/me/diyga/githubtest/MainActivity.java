f9d35483f270eb4a2073a8e3f34eadbfc13c03cd
chris-jeong <chris@diyga.me>
2016-06-03 18:37:30 +0900

    initial commit

---
+++ app/src/main/java/me/diyga/githubtest/MainActivity.java
@@ -0,0 +1,18 @@
+package me.diyga.githubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textview);
+        textView.setText("test version 1");
+    }
+}
